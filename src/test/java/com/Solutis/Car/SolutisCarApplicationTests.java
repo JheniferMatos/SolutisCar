9b44e0c6bcbe93fb974a3b70355365b0ab9af3f9
Jhénifer Matos Mendonça <80738491+JheniferMatos@users.noreply.github.com>
2023-08-14 22:37:19 -0400

    Add files via upload

---
+++ src/test/java/com/Solutis/Car/SolutisCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Solutis.Car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutisCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
