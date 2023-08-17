70ddda8690b0fe35bf015052ecacd8771e066524
Jhénifer Matos <jhenifer.matos@ufms.br>
2023-08-17 07:14:14 -0400

    SwaggerConfig

---
+++ src/main/java/com/Solutis/Car/swagger/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.solutis.car.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select() // Seleciona os endpoints que serão documentados
+                .apis(RequestHandlerSelectors.basePackage("com.solutis.car.controller")) // Filtra por pacote
+                .paths(PathSelectors.any()) // Filtra por path
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Solutis Car API")
+                .description("Documentação da API de acesso aos endpoints do Solutis Car.")
+                .version("1.0")
+                .build();
+    }
+}
